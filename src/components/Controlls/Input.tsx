f6474c9a07f2a3e9131b38b1b8008c6b54478e4f
Chaggai <chaggai.thal+github@gmail.com>
2023-06-09 18:50:43 +0300

    added create and update hooks

--- src/components/Controlls/Input.tsx
+++ src/components/Controlls/Input.tsx
@@ -1,14 +1,16 @@
 type Props = {
   type: "text" | "password" | "email" | "number";
+  name: string;
   placeholder: string;
   className?: string;
 };
-const Input = ({ placeholder, type, className }: Props) => {
+const Input = ({ placeholder, type, name, className }: Props) => {
   return (
     <input
       className={`mb-[18px] h-[57px] rounded-full bg-white px-4 ${
         className ? className : ""
       }`}
+      name={name}
       type={type}
       placeholder={placeholder}
     />
--- src/components/Controlls/Select.tsx
+++ src/components/Controlls/Select.tsx
@@ -4,8 +4,9 @@ type Props = {
   options: string[];
   defaultValue: string;
   className?: string;
+  name: string;
 };
-const Select = ({ options, className, defaultValue }: Props) => {
+const Select = ({ options, className, defaultValue, name }: Props) => {
   const [isSelected, setIsSelected] = useState(false);
 
   return (
@@ -13,10 +14,10 @@ const Select = ({ options, className, defaultValue }: Props) => {
       className={`mb-[18px] h-[57px] rounded-full bg-white px-4 selection:text-red-400 ${
         isSelected ? "text-black" : "text-gray-400"
       } ${className ? className : ""}`}
-      name="profession"
+      name={name}
       id="profession"
+      defaultValue={defaultValue}
       onChange={() => setIsSelected(true)}
-      value={defaultValue}
     >
       <option disabled>{defaultValue}</option>
       {options.map((option) => (
